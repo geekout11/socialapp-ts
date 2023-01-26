6acb53240eb4bfe38e8802fa9e64389552753823
Dominik Gumienny <dominik.gumienny00@gmail.com>
2023-01-26 16:21:51 +0100

    added function that will fech all users

--- store/authStore.ts
+++ store/authStore.ts
@@ -1,12 +1,23 @@
+/* BASIC */
+import axios from 'axios'
+
 /* ZUSTAND */
 import create from 'zustand'
 import { persist } from 'zustand/middleware'
 
+
 const authStore = (set: any) => ({
     userProfile: null,
+    allUsers: [],
 
     addUser: (user: any) => set({ userProfile: user }),
-    removeUser: () => set({ userProfile: null })
+    removeUser: () => set({ userProfile: null }),
+
+    fetchAllUsers: async () => {
+        const response = await axios.get('http://localhost:3000/api/users')
+
+        set({allUsers: response.data})
+    }
 })
 
 const useAuthStore: any = create(
